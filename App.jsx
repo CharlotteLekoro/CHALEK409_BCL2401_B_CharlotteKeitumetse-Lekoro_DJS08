39eb74438770664e30ea1cf94a40a227076f3968
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-06-10 15:33:45 +0200

    code re organising
    I have re organised the code and the folders

--- App.jsx
+++ App.jsx
@@ -1,25 +1,52 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import Vans from "../src/pages/Vans/Vans";
+import Login from "../src/pages/Login";
+import VanDetail from "../src/pages/Vans/VanDetails";
+import Layout from "../src/components/Layout";
+import Dashboard from "../src/pages/Host/Dashboard";
+import Reviews from "../src/pages/Host/Reviews";
+import Income from "../src/pages/Host/Income";
+import HostVans from "../src/pages/Host/HostVans";
+import HostVanDetail from "../src/pages/Host/HostVanDetail";
+import HostVanInfo from "../src/pages/Host/HostVanInfo";
+import HostVanPricing from "../src/pages/Host/HostVanPricing";
+import HostVanPhotos from "../src/pages/Host/HostVanPhotos";
+import NotFound from "../src/pages/NotFound";
+import HostLayout from "../src/components/HostLayout";
+import AuthRequired from "../src/components/AuthRequired";
 
-function App() {
+import "./server";
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="login" element={<Login />} />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-function Home() {
-  return <h1>Hello, React Router!</h1>;
-}
-
-function About() {
-  return <h1>About page goes here! 🎉</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
